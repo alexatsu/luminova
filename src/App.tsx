04b72e9b5f2d955e02c0c1a4e9a49adc9085cb16
Alex <trevan98@gmail.com>
2023-03-10 23:58:34 +0300

    finished nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Container from "@mui/material/Container";
+import { Box, TextField, Typography, InputAdornment, Button } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const navbar = (
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Box sx={{ display: "flex", alignItems: "space-between" }}>
+        <Typography
+          sx={{ marginRight: "20px", marginTop: "5px" }}
+          component={"h1"}
+          fontWeight={"bold"}
+          variant="h6"
+        >
+          Unsplash
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+          <TextField
+            sx={{
+              ":focus": { border: "1px solid #e0e0e0" },
+              "& .MuiInput-underline:before": { borderBottom: "transparent" },
+              "& .MuiInputBase-root:after": { borderBottom: "1px solid #63b8d5" },
+              "& .MuiInputBase-root:hover": { borderBottom: "transparent" },
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
+          />
+        </Box>
+      </Box>
+      <Button
+        sx={{
+          backgroundColor: "lightblue",
+          color: "black",
+          ":hover": { backgroundColor: "rgb(46, 163, 190)" },
+          height: "30px",
+        }}
+        variant={"contained"}
+      >
+        Add a photo
+      </Button>
+    </Box>
+  );
+  return <Container sx={{ margin: "10px auto" }}>{navbar}</Container>;
 }
 
-export default App
+export default App;
