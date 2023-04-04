4a388669131b829f5512224721c67237557b71d2
Alex <trevan98@gmail.com>
2023-04-04 15:50:26 +0300

    wip

--- src/layouts/Hero.tsx
+++ src/layouts/Hero.tsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import ImageCard from "../components/ImageCard";
 import useResizeWidth from "../hooks/useResizeWidth";
-import { Container, ImageList } from "@mui/material";
+import { Container, ImageList, ImageListItem, Typography } from "@mui/material";
+import { imagesStyles } from "../styles/imageCard";
 
 const baseURL = "http://localhost:8080";
+type Images = {
+  id: string;
+  image: string;
+  title: string;
+};
 export default function Hero() {
-  const [img, setImg] = useState<null | string[]>();
+  const [img, setImg] = useState<null | Images[]>();
+  const width = useResizeWidth();
 
   useEffect(() => {
     fetch(`${baseURL}/api/images`)
@@ -13,16 +20,28 @@ export default function Hero() {
       .then((data) => setImg(data))
       .catch((error) => console.error(error));
   }, []);
+  console.log(img, "img");
+
   return (
-    // <Container sx={{ marginTop: "100px" }}>
-    //   <ImageList variant="masonry" cols={width > 568 ? 3 : 1} gap={8}>
-    //     <ImageCard />
-    //   </ImageList>
-    // </Container>
-    <>
-      {img?.map((image) => (
-        <img key={image} src={`${baseURL}/${image}`} alt={image} />
-      ))}
-    </>
+    <Container sx={{ marginTop: "100px" }}>
+      <ImageList variant="masonry" cols={width > 568 ? 3 : 1} gap={8}>
+        <>
+          {img?.map(({ id, image, title }) => (
+            <ImageListItem key={id} sx={imagesStyles.container}>
+              <img
+                key={id}
+                src={`${baseURL}/${image}`}
+                alt={title}
+                loading={"lazy"}
+                style={{ width: "100%", height: "100%" }}
+              />
+              <Typography sx={imagesStyles.title} variant={"h5"}>
+                {title}
+              </Typography>
+            </ImageListItem>
+          ))}
+        </>
+      </ImageList>
+    </Container>
   );
 }
