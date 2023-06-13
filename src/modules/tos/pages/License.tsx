9a5f079d740701c81c13d001f78fa88d2a29a586
Kotatsu1 <nikitakotatsu@gmail.com>
2023-06-13 04:45:28 +0300

    added content

--- src/modules/tos/pages/License.tsx
+++ src/modules/tos/pages/License.tsx
@@ -9,7 +9,14 @@ export function License() {
   return (
     <div>
       <h1>{renderField(content, "header")}</h1>
-      <div>{renderField(content, "luminovaCanBeUsedFreely")}</div>      
+      <div>{renderField(content, "luminovaCanBeUsedFreely")}</div>
+      <h4>{renderField(content, "whatIsAllowed")}</h4>
+      <div>{renderField(content, "allowedList")}</div> 
+      <h4>{renderField(content, "whatIsNotPermitted")}</h4>
+      <h4>{renderField(content, "notPermittedList")}</h4>
+      <h4>{renderField(content, "longform")}</h4>
+      <h4>{renderField(content, "longformText")}</h4>
+      <h4>{renderField(content, "questions")}</h4>
     </div>
   );
 }
