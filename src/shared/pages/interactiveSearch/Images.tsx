9b0fcbadadc5dcae30478aa4ec53135c24d20c9d
Alex <trevan98@gmail.com>
2023-10-05 17:22:39 +0300

    implemented search images, need optimisations

--- src/shared/pages/interactiveSearch/Images.tsx
+++ src/shared/pages/interactiveSearch/Images.tsx
@@ -1,18 +1,21 @@
-import { MasonryImages } from "@/modules/core/layouts";
-import { downloadImage, handleFetch } from "@/shared/utils";
-import { useDebounce, useResizeWidth } from "@/shared/hooks";
 import { useParams } from "react-router-dom";
 
-import { useImages } from "@/modules/core/hooks"; //change position if this file
 import { Resources } from "@/shared/types";
+import { PageWrapper } from "@/shared/layouts";
+import { downloadImage, handleFetch } from "@/shared/utils";
+import { useDebounce, useResizeWidth } from "@/shared/hooks";
+import sass from "@shared/styles/pages/InteractiveSearch/Images.module.scss";
 
-//TODO, store search in global state then refetch it
+import { useImages } from "@/modules/core/hooks"; //change position of this file to shared
+import { MasonryImages } from "@/modules/core/layouts"; //change position of this file to shared
+import { Loader } from "@/shared/components";
+
 export function Images() {
   const { query } = useParams();
   const width = useResizeWidth();
   const { debouncedValue: debouncedWidth } = useDebounce<number>(width, 400);
 
-  const queryKey = ["searchData"];
+  const queryKey = ["searchData", query];
   const getData = async (): Promise<{ images: Resources[] } | undefined> => {
     const url = "http://localhost:8080/search/fornonuser";
 
@@ -25,16 +28,17 @@ export function Images() {
   };
   const { data, status, updateFavoriteImages } = useImages(() => getData(), queryKey);
 
-  console.log(data?.images, "query in images");
-  return (
-    <div>
-      <h2>{query}</h2>
+  return status === "loading" ? (
+    <Loader style={{ margin: " auto " }} />
+  ) : (
+    <PageWrapper>
+      <h2 className={sass.title}>{query}</h2>
       <MasonryImages
         width={debouncedWidth}
         data={data?.images as Resources[]}
         updateFavImages={updateFavoriteImages}
         download={downloadImage}
       />
-    </div>
+    </PageWrapper>
   );
 }
