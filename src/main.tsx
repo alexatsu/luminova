db171cf98d563ff88924952cd568091e1df64299
Alex <trevan98@gmail.com>
2023-10-23 06:08:21 +0300

    up 1

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import { routes } from "./shared/routes";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { baseURL } from "./shared/utils";
+
+console.log(baseURL, "baseURL");
 
 export const queryClient = new QueryClient({
   defaultOptions: {
