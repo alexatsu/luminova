d0032a106316dd1eba09f7a21fe8187f1ee997cd
Alex <trevan98@gmail.com>
2023-10-23 04:42:54 +0300

    about module is partially finished

--- src/modules/company/assets/img/index.ts
+++ src/modules/company/assets/img/index.ts
@@ -2,10 +2,14 @@ import aboutHeader from "./about-header.avif";
 import guyBesideTheWall from "./guyBesideTheWall.avif";
 import community from "./community.avif";
 import girlWithLaptop from "./girlWithLaptop.avif";
+import awesome from "./awesome.avif";
+import freeImg from "./freeImg.avif";
 
 export const aboutImages = {
   aboutHeader,
   guyBesideTheWall,
   community,
   girlWithLaptop,
+  awesome,
+  freeImg
 };
--- src/modules/company/pages/About.tsx
+++ src/modules/company/pages/About.tsx
@@ -2,21 +2,19 @@ import { Link } from "react-router-dom";
 import sass from "../sass/pages/About.module.scss";
 import { aboutImages } from "../assets/img";
 
-const { aboutHeader, guyBesideTheWall, community, girlWithLaptop } = aboutImages;
+const { aboutHeader, freeImg, guyBesideTheWall, awesome, community, girlWithLaptop } = aboutImages;
 
 export const About = () => {
   return (
     <main className={sass.container}>
       <section className={sass.headerSection}>
         <div className={sass.headerText}>
-          <div>
-            <h1>Photos for everyone</h1>
+          <h1>Photos for everyone</h1>
 
-            <p>
-              Over 3 million free high-resolution images brought to you by the world’s most generous
-              community of photographers.
-            </p>
-          </div>
+          <p>
+            Over 3 million free high-resolution images brought to you by the world’s most generous
+            community of photographers.
+          </p>
 
           <Link to="/" className={sass.headerBtn}>
             <button>Start browsing</button>
@@ -59,7 +57,41 @@ export const About = () => {
         </div>
       </section>
 
-      
+      <section className={sass.awesome}>
+        <div className={sass.awesomeText}>
+          <h3>Make something awesome</h3>
+          <p>
+            Luminova was born from the pain we had in finding great, usable imagery. And we weren’t
+            alone. Which is why, today—millions of creators from around the world have downloaded
+            over 4 billion Luminova images to create presentations, artwork, mockups, and more.
+          </p>
+          <Link to={"/"}>
+            <button>Find the perfect image</button>
+          </Link>
+        </div>
+
+        <div>
+          <img alt={"awesome"} src={awesome} />
+        </div>
+      </section>
+
+      <section className={sass.free}>
+        <div className={sass.freeText}>
+          <h3>Is it really free? Yes.</h3>
+          <p>
+            Luminova is a platform powered by an amazing community that has gifted hundreds of
+            thousands of their own photos to fuel creativity around the world. So sign up for free,
+            or don’t. Either way, you’ve got access to over 3 million photos under the Luminova
+            license—which makes them free to do-whatever-you-want with.
+          </p>
+          <Link to={"/tos/license"}>
+            <button>Learn more about the Luminova License</button>
+          </Link>
+        </div>
+        <div>
+          <img alt={"free"} src={freeImg} />
+        </div>
+      </section>
     </main>
   );
 };
